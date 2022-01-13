adfe3983ebdcda5eac7bb3d2b1970c0e06e0a0eb
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-13 18:59:49 -0500

    chore: function prototype about pop opcode

--- monty.h
+++ monty.h
@@ -48,6 +48,7 @@ int my_getline(char **line, int *n, char **buff);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *top);
 
 int is_number(char *s);
