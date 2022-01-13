64c45ec1fe145bc7ef163aa634918378afe567e9
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-13 17:00:34 -0500

    new condition in push function when the argument of push opcode is a string and new defined function

--- opcodes.c
+++ opcodes.c
@@ -11,9 +11,11 @@ void push(stack_t **stack, unsigned int line_number)
 	char *num;
 
 	num = strtok(NULL, DELIMS);
-	if (!num)
+	if (!num || !(is_number(num)))
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		free(buff);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 	new_item = malloc(sizeof(stack_t));
@@ -44,3 +46,17 @@ void pall(stack_t **stack, unsigned int line_number)
 		printf("%d\n", item->n);
 }
 
+/**
+ * pint - prints the value of the stack top
+ * @stack: address of the stack top
+ * @line_number: line number of the opcode
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
