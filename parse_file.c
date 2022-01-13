663d3656837736540ba4009ced85c434f6523a0c
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-01-13 16:53:51 -0500

    refactor: buff is the new global variable and there is a third argument to parse_fle function

--- parse_file.c
+++ parse_file.c
@@ -1,13 +1,16 @@
 #include "monty.h"
 
+char *buff;
+
 /**
  * parse_file - reads the file and retrieves the opcodes
  * @file: name of the file
+ * @top: address of the stack top
  */
-void parse_file(char *file)
+void parse_file(char *file, stack_t **top)
 {
 	int fd, nb_line, pos = 0, sz_buff = 1024;
-	char *line = NULL, *opcode, *buff;
+	char *line = NULL, *opcode;
 	ssize_t sz;
 
 
@@ -26,7 +29,7 @@ void parse_file(char *file)
 	{
 		opcode = strtok(line, DELIMS);
 		if (opcode)
-			match_opcode(opcode, nb_line + 1, buff);
+			match_opcode(opcode, nb_line + 1, top);
 	}
 	free(buff);
 }
